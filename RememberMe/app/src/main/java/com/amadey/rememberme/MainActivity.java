18680437ce465efeb01bc8a9b930064a74fbe195
horologer <YOUR EMAIL ADDRESShorologer.games@gmail.com>
2015-12-18 23:21:20 +0300

    Create MainActivity layout and class

---
+++ RememberMe/app/src/main/java/com/amadey/rememberme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.amadey.rememberme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Amadey on 18.12.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
